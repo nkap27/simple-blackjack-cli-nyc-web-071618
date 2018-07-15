773eed722d704bd90fcb50a4f1164d8659ea16f6
Natasha K <kapr.natasha@gmail.com>
2018-07-15 17:35:01 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -20,7 +20,7 @@ def get_user_input
 end
 
 def end_game(card_total)
-  "Sorry, you hit #{card_total}. Thanks for playing!"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
